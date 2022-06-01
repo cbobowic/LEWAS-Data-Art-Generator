211a7f52ad728db65fa4dec2c448aeced085a35a
michelleramsahoye <60547600+hwangpasta@users.noreply.github.com>
2022-06-01 11:55:46 -0400

    general pseudocode

---
+++ pseudocode.py
@@ -0,0 +1,35 @@
+#Pseudocode
+
+#potential needed libraries
+# pillow/PIL
+# turtle graphics
+# cairo graphics
+# vsketch
+
+
+#Step 1: Need to prompt user for data
+# - Have it run where the user initially provides some data path as a parameter
+# - Make it interactive and have the user do stuff
+
+#Step 2: We have the data
+# - Data needs to be reduced somehow
+#   - Either taking particular columns and assigning that to an array -> memory inefficient?
+#   - The user possibly provides the data reduced at the start
+#   - Libraries of interest - pandas
+#   - Cap on the amount of data that the user can manipulate
+# - Choose to make this generalized or specific for the LEWAS data
+
+#Step 3: Generate images
+# - What do we want it to look like? Shapes, lines
+# - What aspects do we want to change? (Color, size, darkness, location/placement to indicate time?)
+# - The datetime might need be converted into a value? But also ID so like hm?
+# - Multiple types of images (different functions to show different things)
+#   - Pick 1 or 2 attributes that correlate to a specific value that would be under each specific
+#   function, determine which aspects can be changed by the user per function
+
+
+#Step 4: Display
+# - In what format should the images be output? (jpg png, etc.)
+# - Effect of different attributes (salinity, etc.) on wildlife
+# - Incorporate some way for the user to save their image
+# - Pitch some sort of social media something #share...
